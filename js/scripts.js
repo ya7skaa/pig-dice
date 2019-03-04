bf762ba289541cfe96e48cfca8f228a502beeb2e
Yasmin Abdirahman <yasminosman37@yahoo.com>
2019-03-04 17:19:14 +0300

    prototypes created

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,31 @@ this.currentScore = 0;
 this.total = 0;
 this.turn=turn;
 };
+
+Member.prototype.sideOne = function() {
+if (this.roll === 1) {
+  this.currentScore = 0;
+  alert("You rolled 1,Pass to next Player! ");
+} else {
+  this.total += this.roll;
+}
+}
+
+Member.prototype.switch = function() {
+this.total += this.currentScore;
+this.currentScore = 0;
+alert("Next players turn!");
+}
+
+Member.prototype.winner = function() {
+if(this.total >= 100) {
+  alert("Winner!!")
+}
+newRound();
+}
+
+Member.prototype.newRound = function() {
+this.roll = 0;
+this.currentScore = 0;
+this.total = 0;
+}
