cb7d58add248a39f8e30ebbefb3ed1a4ecba5810
Yasmin Abdirahman <yasminosman37@yahoo.com>
2019-03-04 17:18:02 +0300

    constructor for the roll,score and total added

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,10 @@ var memberTwo;
 var rollDice=function() {
 return Math.floor(6*Math.random()) + 1
 }
+
+function Member(turn) {
+this.roll = 0;
+this.currentScore = 0;
+this.total = 0;
+this.turn=turn;
+};
